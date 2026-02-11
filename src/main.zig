168a9038ca7a7aa9e56b19691c8a137c3252b59c
Adrià Arrufat <adria.arrufat@gmail.com>
2026-02-11 20:27:19 +0900

    chore: update zig version and use std.process.Init
    Updates the minimum Zig version to 0.16.0-dev.2535 and refactors the
    main function to use the new `std.process.Init` API. Also fixes a typo
    in the help message.

--- src/main.zig
+++ src/main.zig
@@ -2,26 +2,17 @@ const std = @import("std");
 
 const jxl = @import("jxl.zig");
 
-pub fn main() !void {
-    var threaded_io: std.Io.Threaded = .init_single_threaded;
-    defer threaded_io.deinit();
-    const io = threaded_io.io();
-
-    var debug_allocator: std.heap.DebugAllocator(.{}) = .init;
-    defer _ = debug_allocator.deinit();
-    const gpa = debug_allocator.allocator();
-
-    var args = std.process.args();
+pub fn main(init: std.process.Init) !void {
+    var args = try init.minimal.args.iterateAllocator(init.gpa);
     _ = args.skip();
     if (args.next()) |arg| {
         var image: jxl.Image = .empty;
-        defer image.deinit(gpa);
-        try jxl.load(io, gpa, arg, &image);
+        defer image.deinit(init.gpa);
+        try jxl.load(init.io, init.gpa, arg, &image);
         std.debug.print("image: {}×{}×{} ({} bytes)\n", .{ image.cols, image.rows, image.channels, image.data.len });
         const quality: f32 = if (args.next()) |q| std.fmt.parseFloat(f32, q) catch 100 else 100;
-        try jxl.save(io, gpa, image, "output.jxl", quality);
+        try jxl.save(init.io, init.gpa, image, "output.jxl", quality);
     } else {
-        std.debug.print("pass a JPEG XL and an optiional quality\n", .{});
-        return;
+        std.debug.print("pass a JPEG XL and an optional quality\n", .{});
     }
 }
